7066b4eb9d928cd8a495edb34797473e4fb94ad8
zaid25akhter <zaid25akhter@gmail.com>
2024-12-13 23:25:21 +0530

    checkpoint_01

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,7 +3,38 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
+// function App() {
+//     const [todos, setTodos] = useState([
+//         {
+//             title: "go to gym",
+//             description: "go to gym",
+//             completed: false,
+//         },
+//         {
+//             title: "coding",
+//             description: "coding",
+//             completed: false,
+//         },
+//     ]);
+//     return (
+//         <div>
+//             {todos.map((todo) => (
+//                 <Todo title={todo.title} description={todo.description} />
+//             ))}
+//         </div>
+//     );
+// }
+
+function Todo(props) {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            <h2>{props.description}</h2>
+        </div>
+    );
+}
+
+function App2() {
     const [count, setCount] = useState(0);
 
     return (
@@ -15,4 +46,21 @@ function App() {
     );
 }
 
+function App() {
+    let a = 1;
+    window.setInterval(() => {
+        a++;
+    }, 1000);
+    return (
+        <div>
+            <Child a={a} />
+        </div>
+    );
+}
+function Child(props) {
+    return <div>{props.a}</div>;
+}
 export default App;
+
+//any time parent rerenders then child also
+//rerenders
